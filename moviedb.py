4641fbd874a526adeea7514a5edfcf6d5f90b3b5
AkaiO <akai.outlaw10@gmail.com>
2026-09-16 14:58:51 -0500

    Final Commit: Database Finished

--- moviedb.py
+++ moviedb.py
@@ -1,15 +1,21 @@
 # Movie DB Dictionary Project
+import json  # bring module into program to use it
+
+# Movie Database
 movie_db = {}
 
 # Add a movie
-def add_movie():
-    title = input("Enter the movie title: ")
-    year = input("Enter the movie year ")
-    genre = input("Enter the movie genre: ")
-    director = input("Enter the movie director: ")
-    actors = input("Enter the actors (comma separted): ")
 
-# Create movie in database
+
+def add_movie():
+    # Got data from user and saved into variables
+    title = input('Enter the movie title: ')
+    year = input('Enter the movie year: ')
+    genre = input('Enter the movie genre: ')
+    director = input('Enter the movie director: ')
+    actors = input('Enter the name of actors(comma separated): ')
+
+    # Used data to create a movie in the database
     movie_db[title] = {
         'year': year,
         'genre': genre,
@@ -17,69 +23,139 @@ def add_movie():
         'actors': actors.split(",")
     }
 
-    print(f'{title} successfully added 🙌🏾')
+    print(f'🎉 Success: {title} added!')
 
 # Edit a movie
-def edit_move():
+
+
+def edit_movie():
     # Ask user what movie to edit
-    title = input("Enter the movie title you want to update: ")
+    title = input('Enter the movie title you want to update: ')
 
     try:
-        
-        # find move to edit
+        # find movie to edit
         if title not in movie_db:
-                # if can't find movie keyerror
-                raise KeyError(f"{title} not found in database")
-        
-            # Show current info
-        print(f"Current information for {title}")
+            # if cant find movie keyerror
+            raise KeyError(f'{title} not found in database.')
+
+        # Show current info
+        print(f'Current information for {title}')
         print(movie_db[title])
-        
+
         # Collect updated info from user
-        year = input("Enter the movie year (or press Enter to keep current value(s)): ")
-        genre = input("Enter the movie genre (or press Enter to keep current value(s)): ")
-        director = input("Enter the movie director (or press Enter to keep current value(s)): ")
-        actors = input("Enter the actors (comma separted) (or press Enter to keep current value(s)): ")
+        year = input(
+            'Enter the movie year (or press Enter to keep current value(s)): ')
+        genre = input(
+            'Enter the movie genre (or press Enter to keep current value(s)): ')
+        director = input(
+            'Enter the movie director (or press Enter to keep current value(s)): ')
+        actors = input(
+            'Enter the name of actors(comma separated) (or press Enter to keep current value(s)): ')
 
-        
-            # Update with new information 
+        # Update with new information
+        # Can other datatype than booleans evaluate to true or false
         if year:
-            movie_db[title]["year"] = year
+            movie_db[title]['year'] = year
         if genre:
-            movie_db[title]["genre"] = genre
+            movie_db[title]['genre'] = genre
         if director:
-            movie_db[title]["director"] = director
+            movie_db[title]['director'] = director
         if actors:
-            movie_db[title]["actors"] = actors.split(",")
+            movie_db[title]['actors'] = actors.split(",")
 
-        print(f"{title} has been updated.")
+        print(f'✅ {title} has been updated.')
     except Exception as e:
-        print(f"❌ Error: {e}")
-
+        print(f'❌ Error: {e}')
 
 # Delete a movie
+# declare delete_movie() function
+
+
+def del_movie():
+    # ask the user for the movie to be deleted
+    title = input('Enter movie title to delete: ')
+
+    try:
+        if title not in movie_db:
+            raise KeyError(f'{title} not in database')
+
+        del movie_db[title]
+
+        print(f'🎉 Success. {title} deleted!')
+    # if movie exists, delete from db and print success!
+    except Exception as e:
+        print(f'❌ Error: {e}')
+    # error handle if the movie doesnt exists
+
 
 # View all movies
-def view_movies():
-    print("Showing all movies 🍿")
-    print("========================")
+def show_all():
+    print('⭐️ All movies in Database ⭐️')
+    print('===============')
     for movie in movie_db:
-        print(f"Movie: {movie_db}")
+        print(f"Movie: {movie}")
         for key, value in movie_db[movie].items():
             print(f"{key}: {value}")
-
+        print('===============')
 
 # Search Movies
+# Define a search function
 
-# Save and load movie from a title
 
-# Error handling
+def search_movies():
+    # Prompted the user for search criteria
+    print('🔎 Search Movies in DB')
+    criteria = input('Enter search criteria: ')
+    matches = []  # memory allocation for matching movies
+    ["Dark Knight", 'Spirited Away', 'Forest Gump']
 
-# Data validation
+    # Loop through my db to find matches
+    for movie, info in movie_db.items():
+        # use control flow statement with membership operator to find matches
+        if criteria in movie or criteria in info['director'] or criteria in info['actors'] or criteria in info['genre']:
+            matches.append(movie)  # Add title to list of found movie
 
-# Save Data - put db to file
+    # if movie(s) found
+    if matches:
+        print('✅ Matches Found:')
+        for movie in matches:  # looping over movie titles, dict[key]
+            print(f'{movie}: {movie_db[movie]}')
+    # else no movies found
+    else:
+        print('❌ No matches found.')
 
-# Load Data - pull previous database file into this program 
+# save_data - put db to a file
+
+
+def save_data():
+    # ask name filename to create
+    filename = input('Enter the filename to save to: ')
+    # open file, 'w' mode, put in 'f' variable
+    with open(f'data/{filename}.json', 'w') as f:
+        # Dump data into external file
+        json.dump(movie_db, f)
+    print('🎉 Success, Data Saved!')
+
+# load_data - pull previous database file into this program
+# define load_data function
+    # ask user where to import file from
+    # try to open the file
+    # save file contents to temp database in app.
+    # print success message
+
+
+def load_data():
+    filename = input('Enter the filename to load: ')
+
+    with open(f'data/{filename}.json', 'r') as f:
+        data = json.load(f)
+
+        global movie_db
+        movie_db = data
+
+    print('🎉 Data loaded successfully')
+
+
 # We have to find a way, to repeatly ask the user what action they want to take
 while True:
     print('==== 🎬 Movie Database MGMT System 🎬 ====')
@@ -100,16 +176,16 @@ while True:
     elif choice == '2':
         add_movie()
     elif choice == '3':
-        view_movies()
+        show_all()
     elif choice == '4':
-        edit_move()
+        edit_movie()
     elif choice == '5':
-        print('Deleting a movie')
+        del_movie()
     elif choice == '6':
-        print('Searching for movie')
+        search_movies()
     elif choice == '7':
-        print('Saving data to file')
+        save_data()
     elif choice == '8':
-        print('Loading data from file')
+        load_data()
     else:
         print('❌ Invalid Option. Please try again.')
